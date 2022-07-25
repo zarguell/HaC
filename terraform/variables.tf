350184b0a2abdc7b35c5762fc68ef3933818c142
zarguell <zac.arguelles@gmail.com>
2022-07-24 20:44:13 -0400

    add terrform configs from fvumbaca/terraform-proxmox-k3s

---
+++ terraform/variables.tf
@@ -0,0 +1,146 @@
+variable "proxmox_node" {
+  description = "Proxmox node to create VMs on."
+  type        = string
+}
+
+variable "authorized_keys_file" {
+  description = "Path to file containing public SSH keys for remoting into nodes."
+  type        = string
+}
+
+variable "network_gateway" {
+  description = "IP address of the network gateway."
+  type        = string
+  validation {
+    # condition     = can(regex("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}/[0-9]{1,2}$", var.network_gateway))
+    condition     = can(regex("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}$", var.network_gateway))
+    error_message = "The network_gateway value must be a valid ip."
+  }
+}
+
+variable "lan_subnet" {
+  description = <<EOF
+Subnet used by the LAN network. Note that only the bit count number at the end
+is acutally used, and all other subnets provided are secondary subnets.
+EOF
+  type        = string
+  validation {
+    condition     = can(regex("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}/[0-9]{1,2}$", var.lan_subnet))
+    error_message = "The lan_subnet value must be a valid cidr range."
+  }
+}
+
+variable "control_plane_subnet" {
+  description = <<EOF
+EOF
+  type        = string
+  validation {
+    condition     = can(regex("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}/[0-9]{1,2}$", var.control_plane_subnet))
+    error_message = "The control_plane_subnet value must be a valid cidr range."
+  }
+}
+
+variable "cluster_name" {
+  default     = "k3s"
+  type        = string
+  description = "Name of the cluster used for prefixing cluster components (ie nodes)."
+}
+
+variable "node_template" {
+  type        = string
+  description = <<EOF
+Proxmox vm to use as a base template for all nodes. Can be a template or
+another vm that supports cloud-init.
+EOF
+}
+
+variable "proxmox_resource_pool" {
+  description = "Resource pool name to use in proxmox to better organize nodes."
+  type        = string
+  default     = ""
+}
+
+variable "support_node_settings" {
+  type = object({
+    cores          = optional(number),
+    sockets        = optional(number),
+    memory         = optional(number),
+    storage_type   = optional(string),
+    storage_id     = optional(string),
+    disk_size      = optional(string),
+    user           = optional(string),
+    db_name        = optional(string),
+    db_user        = optional(string),
+    network_bridge = optional(string),
+    network_tag    = optional(number), 
+  })
+}
+
+variable "master_nodes_count" {
+  description = "Number of master nodes."
+  default     = 2
+  type        = number
+}
+
+variable "master_node_settings" {
+  type = object({
+    cores          = optional(number),
+    sockets        = optional(number),
+    memory         = optional(number),
+    storage_type   = optional(string),
+    storage_id     = optional(string),
+    disk_size      = optional(string),
+    user           = optional(string),
+    network_bridge = optional(string),
+    network_tag    = optional(number),
+  })
+}
+
+variable "node_pools" {
+  description = "Node pool definitions for the cluster."
+  type = list(object({
+
+    name   = string,
+    size   = number,
+    subnet = string,
+
+    taints = optional(list(string)),
+
+    cores        = optional(number),
+    sockets      = optional(number),
+    memory       = optional(number),
+    storage_type = optional(string),
+    storage_id   = optional(string),
+    disk_size    = optional(string),
+    user         = optional(string),
+    network_tag  = optional(number),
+
+    template = optional(string),
+
+    network_bridge = optional(string),
+  }))
+}
+variable "api_hostnames" {
+  description = "Alternative hostnames for the API server."
+  type        = list(string)
+  default     = []
+}
+
+variable "k3s_disable_components" {
+  description = "List of components to disable. Ref: https://rancher.com/docs/k3s/latest/en/installation/install-options/server-config/#kubernetes-components"
+  type        = list(string)
+  default     = []
+}
+
+
+variable "http_proxy" {
+  default     = ""
+  type        = string
+  description = "http_proxy"
+}
+
+variable "nameserver" {
+  default     = ""
+  type        = string
+  description = "nameserver"
+}
---
+++ terraform/worker_nodes.tf
@@ -0,0 +1,115 @@
+resource "macaddress" "k3s-workers" {
+  for_each = local.mapped_worker_nodes
+}
+
+locals {
+
+  listed_worker_nodes = flatten([
+    for pool in var.node_pools :
+    [
+      for i in range(pool.size) :
+      merge(defaults(pool, {
+        cores          = 2
+        sockets        = 1
+        memory         = 4096
+        storage_type   = "scsi"
+        storage_id     = "local-lvm"
+        disk_size      = "20G"
+        user           = "k3s"
+        template       = var.node_template
+        network_bridge = "vmbr0"
+        network_tag    = -1
+        }), {
+        i  = i
+        ip = cidrhost(pool.subnet, i)
+      })
+    ]
+  ])
+
+  mapped_worker_nodes = {
+    for node in local.listed_worker_nodes : "${node.name}-${node.i}" => node
+  }
+
+}
+
+resource "proxmox_vm_qemu" "k3s-worker" {
+  depends_on = [
+    proxmox_vm_qemu.k3s-support,
+    proxmox_vm_qemu.k3s-master,
+  ]
+
+  for_each = local.mapped_worker_nodes
+
+  target_node = var.proxmox_node
+  name        = "${var.cluster_name}-${each.key}"
+
+  clone = each.value.template
+
+  pool = var.proxmox_resource_pool
+
+  # cores = 2
+  cores   = each.value.cores
+  sockets = each.value.sockets
+  memory  = each.value.memory
+
+  agent = 1
+
+  disk {
+    type    = each.value.storage_type
+    storage = each.value.storage_id
+    size    = each.value.disk_size
+  }
+
+  network {
+    bridge    = each.value.network_bridge
+    firewall  = true
+    link_down = false
+    macaddr   = upper(macaddress.k3s-workers[each.key].address)
+    model     = "virtio"
+    queues    = 0
+    rate      = 0
+    tag       = each.value.network_tag
+  }
+
+  lifecycle {
+    ignore_changes = [
+      ciuser,
+      sshkeys,
+      disk,
+      network
+    ]
+  }
+
+  os_type = "cloud-init"
+
+  ciuser = each.value.user
+
+  ipconfig0 = "ip=${each.value.ip}/${local.lan_subnet_cidr_bitnum},gw=${var.network_gateway}"
+
+  sshkeys = file(var.authorized_keys_file)
+
+  nameserver = var.nameserver
+
+  connection {
+    type = "ssh"
+    user = each.value.user
+    host = each.value.ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      templatefile("${path.module}/scripts/install-k3s-server.sh.tftpl", {
+        mode         = "agent"
+        tokens       = [random_password.k3s-server-token.result]
+        alt_names    = []
+        disable      = []
+        server_hosts = ["https://${local.support_node_ip}:6443"]
+        node_taints  = each.value.taints
+        datastores   = []
+
+        http_proxy  = var.http_proxy
+      })
+    ]
+  }
+
+}
